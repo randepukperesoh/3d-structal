6054aa2127f14f700f225daa6f8e74f815153bb2
randepukperesoh <mirgthebest@gmail.com>
2023-01-31 16:58:26 +0300

    add screenshot creator

--- src/components/Canvas/Area.js
+++ src/components/Canvas/Area.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Dot from './Dot'
 import Kernel from './Kernel';
@@ -6,6 +6,7 @@ import Controls from './Controls'
 import GridPlane from './GridPlane'
 import { useSelector, useDispatch } from 'react-redux'
 import { addKernel, addNode } from '../store/Slice';
+import html2canvas from "html2canvas"
 
 export default function Area() {
     const nodes = useSelector(state => state.nodes.nodes);
@@ -13,7 +14,13 @@ export default function Area() {
     const config = useSelector(state => state.nodes.config)
     const dispatch = useDispatch();
 
-    
+    const cnvs = useRef(null);
+
+    //useEffect( () => {
+    //    fetch('http://localhost:8080/')
+    //    .then((response) => response.json())
+    //    .then((data) => console.log(data));
+    //}, [])
 
     let arrKernels = kernels.map( ( kernel ) => <Kernel key = { kernel.id + 'K' } startEnd = { kernel }/>)
 
@@ -69,8 +76,21 @@ export default function Area() {
         }
     }
 
+    async function handleDownloadImage () {
+        const canvas = await html2canvas(cnvs.current),
+        data = canvas.toDataURL('image/jpg'),
+        link = document.createElement('a');
+     
+        link.href = data;
+        link.download = 'downloaded-image.jpg';
+     
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      };
+
     return(
-        <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [7, 5, 0] }}>
+        <Canvas gl={{ preserveDrawingBuffer: true }} onClick={() => handleDownloadImage()} ref={cnvs} camera={{ fov: 75, near: 0.1, far: 1000, position: [7, 5, 0] }}>
             <Controls />
             <ambientLight intensity={0.5}/>
             <pointLight position={[10, 10, 10]}/>
